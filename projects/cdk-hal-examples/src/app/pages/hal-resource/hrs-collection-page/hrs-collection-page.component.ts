274c5e060062d637e9fc44ab64c03a9623ad3dab
Cheng <jcheng42@dxc.com>
2020-06-15 16:22:32 +0200

    Binding hal resourceService into component properties

--- projects/cdk-hal-examples/src/app/pages/hal-resource/hrs-collection-page/hrs-collection-page.component.ts
+++ projects/cdk-hal-examples/src/app/pages/hal-resource/hrs-collection-page/hrs-collection-page.component.ts
@@ -1,40 +1,51 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { HalResourceService } from '../../services/diaas-angular-cdk-hal.service';
-import { HalResourceServiceFactoryProvider } from '../../../../../../diaas-angular-cdk-hal/src/lib/diaas-angular-cdk-hal.factory-provider';
+import { Component, OnInit, Inject } from "@angular/core";
+import { HalResourceService } from "../../services/diaas-angular-cdk-hal.service";
+import { HttpHeaders, HttpClient } from "@angular/common/http";
+import { HalResourceServiceFactoryProvider } from "../../../../../../diaas-angular-cdk-hal/src/lib/diaas-angular-cdk-hal.factory-provider";
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: 'app-hrs-collection-page',
-  templateUrl: './hrs-collection-page.component.html',
-  styleUrls: ['./hrs-collection-page.component.scss'],
-  providers: [HalResourceServiceFactoryProvider.createInstance(
-    'CollectionProspectService',
-    'https://api.dxc-dev-integral.hub-1.dev.us.insurance.dxc.com/prospects/5e044795cff47e0009e03ac5/telephones/1',
-    {"x-api-key":"F9Pl5g8RlA4bx7NkCYfIP6lEM78Gwage4ZTURipo"})]
+  selector: "app-hrs-collection-page",
+  templateUrl: "./hrs-collection-page.component.html",
+  styleUrls: ["./hrs-collection-page.component.scss"],
+  providers: [HalResourceService]
 })
 export class HrsCollectionPageComponent implements OnInit {
+  fetchStatus: BehaviorSubject<string> = new BehaviorSubject('');
+  error: BehaviorSubject<any> = new BehaviorSubject(null);
+  items: BehaviorSubject<Array<any>> = new BehaviorSubject([]);
+  collectionPropectService: HalResourceService;
 
-  fetchStatus = this.collectionPropectService.fetchStatus;
-  resource = this.collectionPropectService.resource;
-  error = this.collectionPropectService.errorMessage;
-  items = this.collectionPropectService.items;
-
-  constructor(@Inject('CollectionProspectService') private collectionPropectService: HalResourceService) { 
+  constructor(
+    httpClient: HttpClient
+  ) {
+    this.collectionPropectService = new HalResourceService(
+      "https://api.dxc-dev-integral.hub-1.dev.us.insurance.dxc.com/prospects/5e044795cff47e0009e03ac5/telephones",
+      new HttpHeaders({
+        "x-api-key": "F9Pl5g8RlA4bx7NkCYfIP6lEM78Gwage4ZTURipo"
+      }),
+      httpClient
+    );
+    this.fetchStatus = this.collectionPropectService.fetchStatus;
+    this.error = this.collectionPropectService.errorMessage;
+    this.items = this.collectionPropectService.items;
     this.collectionPropectService.fetchResource();
   }
 
-  ngOnInit() {
+  ngOnInit() {}
 
-  }
-
-  navigate(operation:string){
+  navigate(operation: string) {
     this.collectionPropectService.executeItemsHandler(operation);
   }
 
-  getItemPropertyValue(item, propertyKey){
-    return item !== undefined && item !== null && item.summary!== null && item.summary && item.summary[propertyKey] !== null && item.summary[propertyKey] !== undefined ? 
-      item.summary[propertyKey]: '';
+  getItemPropertyValue(item, propertyKey) {
+    return item !== undefined &&
+      item !== null &&
+      item.summary !== null &&
+      item.summary &&
+      item.summary[propertyKey] !== null &&
+      item.summary[propertyKey] !== undefined
+      ? item.summary[propertyKey]
+      : "";
   }
-
-
-
 }
