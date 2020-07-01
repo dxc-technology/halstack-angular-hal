b9f332f4f55d8219191124f74a84cc0df5ff93ce
agonzalez97 <aidamaria.gonzalez@dxc.com>
2020-07-01 18:38:31 +0200

    Mocking http call

--- projects/diaas-angular-cdk-hal/src/lib/table/table.spec.ts
+++ projects/diaas-angular-cdk-hal/src/lib/table/table.spec.ts
@@ -2,14 +2,19 @@ import { render, waitForElement } from '@testing-library/angular';
 import { screen, waitFor } from "@testing-library/dom";
 import { DxcHalTable } from './table';
 import { CdkTableModule } from './table-module';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { setupServer } from 'msw/node';
 
 import { rest } from 'msw';
 import data from "./mocks/tableResponseMock";
 
-/*const handlers = [
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { HalResourceService } from './services/diaas-angular-cdk-hal.service';
+import { of } from 'rxjs';
+import { TestBed, getTestBed } from '@angular/core/testing';
+
+const handlers = [
   rest.get('http://localhost:3000/data?_start=1&_num=4', async (req, res, ctx) => {
     return res(ctx.status(200),ctx.json(data))
   }),
@@ -19,19 +24,34 @@ const server = setupServer(...handlers);
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
-afterAll(() => server.close())*/
+afterAll(() => server.close())
 
-import 'jest-preset-angular'
+/*import 'jest-preset-angular'
 import { server } from './mocks/node'
-import { HalResourceService } from './services/diaas-angular-cdk-hal.service';
-import { of } from 'rxjs';
+import { getTestBed, TestBed } from '@angular/core/testing';
+
 
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+afterAll(() => server.close())*/
 
-describe('DxcHalTable tests', () => {
+/*describe('DxcHalTable tests', () => {
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
 
+  beforeEach(() => {
+      TestBed.configureTestingModule({
+          imports: [HttpClientTestingModule],
+          providers: [],
+      });
+      injector = getTestBed();
+      httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+      httpMock.verify();
+  });
+  
   test('should render dxc-hal-table', async () => {
     const hal = await render(DxcHalTable, {
         template:`  <dxc-hal-table halUrl="http://localhost:3000/data" [headers]="" [itemsPerPage]="4">
@@ -53,6 +73,9 @@ describe('DxcHalTable tests', () => {
        ],
        excludeComponentDeclaration: true
     })
+    const req = httpMock.expectOne(`http://localhost:3000/data?_start=1&_num=4`);
+    expect(req.request.method).toBe('GET');
+    req.flush(data);
     await waitForElement(() => hal.getByText(/Loading/i));
     await waitForElement(() => hal.getByRole("table"));
     const table = hal.getByRole("table");
@@ -61,6 +84,56 @@ describe('DxcHalTable tests', () => {
     hal.detectChanges();
     expect(await screen.getByText("BOCJT FJUUZ")).toBeTruthy();
 
+  });
+
+});*/
+
+describe('Hal table', () => {
+  let injector: TestBed;
+  let httpMock: HttpTestingController;
+  
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+  });
+  
+  afterEach(() => {
+    httpMock.verify();
+  });
+  test('should render dxc-hal-table', async () =>{
+    const hal = await render(DxcHalTable, {
+      template:`  <dxc-hal-table halUrl="http://localhost:3000/data" [headers]="" [itemsPerPage]="4">
+                      <ng-container dxcColumnDef="User" [sortable]="{isSortable:true, propertyName:'prospect-full-name'}">
+                          <td *dxcCellDef="let item"> BOCJT FJUUZ </td>
+                      </ng-container>                
+                      <ng-container dxcColumnDef="Email">
+                          <td *dxcCellDef="let element">  </td>
+                      </ng-container>
+                  </dxc-hal-table>`,
+     imports: [CdkTableModule, HttpClientTestingModule, HttpClientModule],
+     providers:[
+      {
+        provide: HalResourceService,
+        useValue: {
+          handleGet: () => of(data),
+        },
+      },
+     ],
+     excludeComponentDeclaration: true
+  });    
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+    const req = httpMock.expectOne(`http://localhost:3000/data?_start=1&_num=4`);
+    expect(req.request.method).toBe('GET');
+    req.flush(data);
+    await waitForElement(() => hal.getByText(/Loading/i));
+    await waitForElement(() => hal.getByRole("table"));
+    const table = hal.getByRole("table");
+    expect(table).toBeTruthy();
+    hal.detectChanges();
+    expect(await screen.getByText("BOCJT FJUUZ")).toBeTruthy();
   });
 
 });
