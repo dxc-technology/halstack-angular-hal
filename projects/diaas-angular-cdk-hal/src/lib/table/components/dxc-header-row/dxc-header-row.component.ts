36bae52f427d84da04948de21d425b6c45f518d6
agonzalez97 <aidamaria.gonzalez@dxc.com>
2021-03-22 17:34:56 +0100

    Fixed sorting hal table + fixed pagination hal table

--- projects/diaas-angular-cdk-hal/src/lib/table/components/dxc-header-row/dxc-header-row.component.ts
+++ projects/diaas-angular-cdk-hal/src/lib/table/components/dxc-header-row/dxc-header-row.component.ts
@@ -1,16 +1,32 @@
-import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
-import { SortService } from '../../services/sort.service';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  ViewEncapsulation,
+} from "@angular/core";
+import { SortService } from "../../services/sort.service";
+import { ElementRef } from '@angular/core';
 
 @Component({
-  selector: 'th',
-  template: `<div ordering="{{isSortable}}" id="header-{{columnName}}-{{parentClassName}}" propertyname={{value}}>{{columnName}} <span id="iconSort-{{columnName}}-{{parentClassName}}" *ngIf="isSortable"></span></div>`,
+  selector: "th",
+  template: `<div
+    ordering="{{ isSortable }}"
+    id="header-{{ columnName }}-{{ parentClassName }}"
+    propertyname="{{ value }}"
+    [ngClass]="[isSortable ? 'isSortable' : '']"
+  >
+    {{ columnName }}
+    <span
+      id="iconSort-{{ columnName }}-{{ parentClassName }}"
+      *ngIf="isSortable"
+    ></span>
+  </div>`,
+  styleUrls: ["./dxc-header-row-component.scss"],
   changeDetection: ChangeDetectionStrategy.Default,
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class DxcHeaderRowComponent {
-
-  columnName:string;
-  isSortable:boolean;
+  columnName: string;
+  isSortable: boolean;
 
   defaultSort: string;
   ascSort: string;
@@ -20,43 +36,35 @@ export class DxcHeaderRowComponent {
   parentClassName: any;
   value: string;
 
-  constructor(private sortService: SortService){}
+  constructor(
+    private sortService: SortService,
+    public elementRef: ElementRef
+  ) {}
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.setSortIcon();
-    this.setStyle();
-  }
-
-  /** Set style for sort header */
-  private setStyle(){
-    if(this.isSortable){
-      let divHeader = document.getElementById(`header-${this.columnName}-${this.parentClassName}`);
-      divHeader.style.cursor = "pointer";
-      divHeader.style.width = "fit-content";
-      divHeader.setAttribute("propertyName", this.value);
-    }
   }
 
   /** Paint icon for sorting depending on header state (up for asc, down for desc and default) */
-  setSortIcon(){
-    if(this.isSortable){
-      let divHeader = document.getElementById(`header-${this.columnName}-${this.parentClassName}`);
-      let spanIcon = document.getElementById(`iconSort-${this.columnName}-${this.parentClassName}`);
-      switch(this.state){
+  setSortIcon() {
+    if (this.isSortable) {
+      let divHeader = this.elementRef.nativeElement;
+      let spanIcon = (divHeader as HTMLElement).getElementsByTagName("span")[0];
+      switch (this.state) {
         case "up":
           let up = this.sortService.getAscIcon(this.columnName);
-          spanIcon.insertAdjacentHTML('beforeend', up);
-          divHeader.setAttribute("state",this.state);
+          spanIcon.insertAdjacentHTML("beforeend", up);
+          divHeader.setAttribute("state", this.state);
           break;
         case "down":
           let down = this.sortService.getDescIcon(this.columnName);
-          spanIcon.insertAdjacentHTML('beforeend', down);
-          divHeader.setAttribute("state",this.state);
+          spanIcon.insertAdjacentHTML("beforeend", down);
+          divHeader.setAttribute("state", this.state);
           break;
         default:
           let defaultIcon = this.sortService.getDefaultIcon(this.columnName);
-          spanIcon.insertAdjacentHTML('beforeend', defaultIcon);
-          divHeader.setAttribute("state",this.state);
+          spanIcon.insertAdjacentHTML("beforeend", defaultIcon);
+          divHeader.setAttribute("state", this.state);
           break;
       }
     }
